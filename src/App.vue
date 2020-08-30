217c224ebc24059be696b9ad0af9f68532b8e0e4
SereninSparks <sereninsparks@gmail.com>
2020-08-30 16:40:05 +0200

    Added loading text

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
     <div class="flex">
         <Suspense>
-            <div class="w-full md:w-1/2 xl:w-1/3">
-                <VoidTrader />
-            </div>
+            <template #default>
+                <div class="w-full md:w-1/2 xl:w-1/3">
+                    <VoidTrader />
+                </div>
 
-            <div class="w-full md:w-1/2 xl:w-1/3">
-                <CambionCycle />
-            </div>
+                <div class="w-full md:w-1/2 xl:w-1/3">
+                    <CambionCycle />
+                </div>
+            </template>
+
+            <template #fallback>
+                <div class="w-full">
+                    <card>
+                        <template v-slot:header>
+                            Loading...
+                        </template>
+
+                        Ordis is ea- <strong>getting tired of waiting</strong> -waiting patiently for the API, Operator!
+                    </card>
+                </div>
+            </template>
         </Suspense>
     </div>
 </template>
 
 <script lang="ts">
 import CambionCycle from './components/CambionCycle.vue';
+import Card from './components/Card.vue';
 import VoidTrader from './components/VoidTrader.vue';
 
 export default {
     name: 'App',
     components: {
+        Card,
         CambionCycle,
         VoidTrader,
     },
